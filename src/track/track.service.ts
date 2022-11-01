34cad4a2aab70c6e148457d5e6560f58059a6f76
Ilias Iangurazov <ilias.iangurazov@epam.com>
2022-11-01 14:35:50 +0600

    created: empty server project

---
+++ src/track/track.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class TrackService {
+    create() {
+
+    }
+
+    getAll() {
+
+    }
+
+    getOne() {
+
+    }
+
+    delete() {
+        
+    }
+}
